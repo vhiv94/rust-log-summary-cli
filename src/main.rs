6243c390e6f94ea0f135d34e1f5b7fb0d96dc367
vhiv94 <vhiv94@gmail.com>
2026-04-06 03:57:25 -0500

    Finalize the logo

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,31 @@
 // Ascii art logo!!!
 // raw string: r#""#
 const LOGO: &str = r#"
-   __               ____                              
-  / /  ___  ___ _  / __/_ ____ _  __ _  ___ _______ __
- / /__/ _ \/ _ `/ _\ \/ // /  ' \/  ' \/ _ `/ __/ // /
-/____/\___/\_, / /___/\_,_/_/_/_/_/_/_/\_,_/_/  \_, / 
-          /___/                                /___/  
-"#; // trim leading and trailing \n
-const RULE: &str = "--------------------------------------------------------------------------------";
+============================================================
+      __               ____                              
+     / /  ___  ___ _  / __/_ ____ _  __ _  ___ _______ __
+    / /__/ _ \/ _ `/ _\ \/ // /  ' \/  ' \/ _ `/ __/ // /
+   /____/\___/\_, / /___/\_,_/_/_/_/_/_/_/\_,_/_/  \_, / 
+             /___/                                /___/
+
+============================================================
+"#;
+// const RULE: &str = "--------------------------------------------------------------------------------";
+const DESC: &str = "         A simple CLI for sumarising server logs";
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// Entry Point
 fn main() {
+    print_logo();
+}
+
+fn print_logo(){
     // \x1B[2J (or \x1B[J) clears the screen.
     // \x1B[1;1H (or \x1B[H) moves the cursor to the top-left corner.
     print!("{}[2J{}[1;1H", 27 as char, 27 as char);
-
+    
     // macro: expands to interperate variable, print the line to stdout, then an additional \n
-    println!("{}", RULE);
     println!("{}", LOGO);
-    println!("{}", RULE);
+    println!("{}", DESC);
+    println!("---------------------------{}---------------------------", VERSION);
 }
